a0a1085e2d3a1d66ac6657a5390a77cf49a743f3
Yuriy Syrovetskiy <cblp@cblp.su>
2020-10-04 22:59:10 +0300

    Add remote work option

--- Main.hs
+++ Main.hs
@@ -319,7 +319,7 @@ cv = CV
 
   , residence = do
       p $ toHtml moscow
-      p "I'm open to relocation." -- to Europe or North America."
+      p "I'm open to remote work and relocation." -- to Europe or North America."
   }
 
   where
